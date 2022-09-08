5f43a2f8770a17a30b237b19706688499d787b8d
Abdullah Roshan <abdullah.roshan@eutec.org>
2022-09-08 03:21:36 -0700

    morsecommit

---
+++ morse/morsecode.py
@@ -0,0 +1,29 @@
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+def translate(words):
+    return " ".join(MORSE_CODE_DICT[char] for char in words.upper())
+    
+
+if __name__ == "__main__":
+    print("This program translates words into to morse code.\n")
+
+    words = input("Enter a word or phrase to be encoded: ")
+
+    try:
+        print(translate(words))
+    except KeyError:
+        print("The word or phrase you've provided contains invalid characters - we can only convert the letters A-Z, numbers, and spaces to Morse Code in this program.")
